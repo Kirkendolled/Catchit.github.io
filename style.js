a32a167663ba72753befc3339d622f9e06ae53d0
Kirkendolled <112414443+Kirkendolled@users.noreply.github.com>
2022-10-23 08:18:08 -0500

    Button

---
+++ style.js
@@ -0,0 +1,13 @@
+const btn=document.querySelector("button");
+    let position=0;
+    btn.addEventListener("mouseover", () => {
+        position = position === 0 ? 150 : 0 ;
+        btn.style.transform=`translate(0,${position}px)`;
+    });
+
+    
+
+  
+
+    
+    
